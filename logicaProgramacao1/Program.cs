c57713baf118fed77dd3a4a8667d013707d2eb56
Ana Araujo <anaregina305@gmail.com>
2020-07-09 23:31:42 -0300

    first commit

---
+++ logicaProgramacao1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace LogicaProgramacao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Operadores relacionais e lógicos
+            bool souRico = false;
+            double salario = 2000;
+
+            if (souRico || salario >= 1000)
+            {
+                Console.WriteLine("Voce é rico");
+            }
+
+
+
+            //string sobremesa = "Pudim";
+            //bool estouAcimaDoPeso = false;
+
+            //if (estouAcimaDoPeso || sobremesa == "Pudim")
+            //{
+            //    Console.WriteLine("Comer sobremesa");
+            //}
+              
+               
+
+            //bool termineiTrabalho = false;
+            //bool estouComFome = true;
+
+            //if (!termineiTrabalho && estouComFome)
+            //{
+            //    Console.WriteLine("Almoçar!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Continuar trabalhando");
+            //}
+
+
+
+            //int numero = 10;
+            //bool boolean = true;
+
+            //if (numero != 10 && boolean == true)
+            //{
+
+            // Console.WriteLine("A condição é verdadeira!");
+            //}
+            //else
+            //{
+            // Console.WriteLine("A condição é falsa!");
+            //}
+            
+            
+
+
+            //Aprendendo sobre concatenação
+            //string nome = "Ana";
+            //string idade = "20";
+            //string bemVindo = $"Seja bem vindo, {nome}. Sua idade é de: {idade} anos";
+            //// string nome = "Ana";
+            ////string frase = bemVindo + nome;
+            //Console.WriteLine(bemVindo);
+                        
+            //Expressoes Logicas e constantes
+            //const int constInteiro = 20;
+            //int inteiro1 = 10;
+            //int inteiro2 = 20;
+            //int soma = inteiro1 + inteiro2;
+            //Console.WriteLine(soma);
+            //inteiro1 = 50 + soma;
+            //Console.WriteLine(inteiro1 * 2);
+
+            //Aprendendo a declarar variaveis
+            //string hello = "Seja bem vindo a nossa aula!";
+            //int inteiro = 1;
+            //double numero = 1.5;
+            //bool booleano = false;
+
+            //Aprendendo sobre Arrays
+            //int[] numeros = new int[5];
+            //numeros[0] = 10;
+            //numeros[1] = 20;
+            //numeros[2] = 50;
+            //numeros[3] = 100;
+            //numeros[4] = 200;
+
+            
+        }
+    }
+}
